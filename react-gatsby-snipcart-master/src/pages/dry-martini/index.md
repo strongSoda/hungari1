66a2d6460bf720374c56c7349a89ae7d89ccb36e
strongSoda <strongsoda2@gmail.com>
2019-03-05 21:14:17 +0530

    init

---
+++ react-gatsby-snipcart-master/src/pages/dry-martini/index.md
@@ -0,0 +1,19 @@
+---
+title: Dry Martini
+layout: post
+date: 2018-07-21
+id: 3
+image:
+    - name: "default"
+      src: "dry-martini"
+
+price: 8.50
+path: "/dry-martini/"
+customFields:
+    name: Size
+    values: ['Classic', 'Large', 'Gatsby-esque']
+
+description: "The taste of excellence."
+---
+
+> "One Martini is all right. Two are too many, and three are not enough."
---
+++ react-gatsby-snipcart-master/src/pages/index.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import { Link, graphql } from 'gatsby'
+import get from 'lodash/get'
+import Helmet from 'react-helmet'
+
+import Bio from '../components/Bio'
+import Layout from '../components/Layout'
+import { rhythm } from '../utils/typography'
+
+class BlogIndex extends React.Component {
+  render() {
+    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
+    const siteDescription = get(
+      this,
+      'props.data.site.siteMetadata.description'
+    )
+    const posts = get(this, 'props.data.allMarkdownRemark.edges')
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <Helmet
+          htmlAttributes={{ lang: 'en' }}
+          meta={[{ name: 'description', content: siteDescription }]}
+          title={siteTitle}/>
+
+        {posts.map(({ node }) => {
+          const title = get(node, 'frontmatter.title') || node.fields.slug;
+          var image = get(node, 'frontmatter.image');
+          const imgSrc= require(`./../pages${node.frontmatter.path}${image[0].src}.jpg`);
+          
+          return (
+            <div key={node.fields.slug}>
+              <h3
+                style={{
+                  marginBottom: rhythm(1 / 4),
+                }}
+              >
+                <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
+                  {title}
+                </Link>
+              </h3>
+              <small>{node.frontmatter.date}</small>
+              
+              <div>
+                <img src={imgSrc} width="200px"></img>
+                <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+              </div>
+            </div>
+          )
+        })}
+      </Layout>
+    )
+  }
+}
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            path
+            image {
+              name
+              src
+            }
+          }
+        }
+      }
+    }
+  }
+`
